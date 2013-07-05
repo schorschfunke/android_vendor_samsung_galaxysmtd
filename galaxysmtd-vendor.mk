dc3f6131d4b1c99c8f92f3332e4d669b6dac2507
schorschfunke <schorsch.funke@googlemail.com>
2013-07-05 06:50:36 +0200

    Add NSTools package

--- galaxysmtd-vendor.mk
+++ galaxysmtd-vendor.mk
@@ -18,7 +18,8 @@ PRODUCT_PACKAGES := \
     LiveWallpapersPicker \
     MagicSmokeWallpapers \
     VisualizationWallpapers \
-    librs_jni
+    librs_jni \
+    NSTools
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES := \
